044025dc547c01174857fc913b5a912446a96668
pillasandeep <155517436+pillasandeep@users.noreply.github.com>
2025-07-11 11:24:01 +0530

    Create calculator.rs

---
+++ Unit3/calculator.rs
@@ -0,0 +1,15 @@
+fn main() {
+       let a=10;
+       let b=65;
+       let o='*';
+       match o
+       {
+           '+'=>print!("{}",a+b),
+           '-'=>print!("{}",a-b),
+           '/'=>print!("{}",a/b),
+           '*'=>print!("{}",a*b),
+           _=>print!("Operator not found"),
+       }
+    
+    
+}
